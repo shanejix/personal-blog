fbd8de2ec1ae97e6f915f61962c044594ee0e1ee
shanejix <shanejix@vip.163.com>
2020-06-15 22:55:10 +0800

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,10 @@ import { useStaticQuery, Link, graphql } from "gatsby"
 import { rhythm } from "../utils/typography"
 
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <li css={css`
+    display: inline-block;
+    margin-right: 1rem 
+  `}>
     <Link to={props.to}>{props.children}</Link>
   </li >
 )
@@ -28,52 +31,37 @@ export default ({ children }) => {
     `
   )
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-      {/* // css use style */}
+    // css in js
+    <div
+      css={css`
+        margin: 3rem auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+     `}
+    >
       <header style={{ marginBottom: `1.5rem` }}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}</h3>
+        <Link to="/" css={css` 
+          text-shadow: none; 
+          background-image: none 
+        `}>
+          <h3
+            css={css`
+                margin-bottom: ${rhythm(2)};
+                display: inline;
+                font-style: normal;
+             `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
         </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
+        <ul css={css` list-style: none; float: right `}>
           <ListLink to="/">Home</ListLink>
           <ListLink to="/about/">About</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
         </ul>
       </header>
       {children}
-
-      {/* // css in js use emotion*/}
-
-      {/* TODO:css in js 放上面不生效 */}
-      <div
-        css={css`
-        margin: 0 auto;
-        max-width: 700px;
-        padding: ${rhythm(2)};
-        padding-top: ${rhythm(1.5)};
-      `}
-      >
-        <Link to={`/`}>
-          <h3
-            css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-          `}
-          >
-            Pandas Eating Lots
-        </h3>
-        </Link>
-        <Link
-          to={`/about/`}
-          css={css`
-          float: right;
-        `}
-        >
-          About
-      </Link>
-        {children}
-      </div>
     </div >
   )
 }
