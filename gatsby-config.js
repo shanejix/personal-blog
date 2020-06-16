22e73425f54dd191eda48d6aa5276cef37d5d0a8
shanejix <shanejix@vip.163.com>
2020-06-16 23:20:26 +0800

    add PWA and SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,39 @@
  */
 
 module.exports = {
-  // 网站元数据
+  // 页面元数据（metadata）:帮助 Google 之类的搜索引擎理解页面内容，决定什么时候出现在搜索结果里的关键。
   siteMetadata: {
-    title: `Title from siteMetadata`,
+    title: `ShaneJixx`,
     author: `shanejixx`,
-    contact: `github.com/shanejixx`,
+    description: `A simple description about author lots...`,
   },
   /* Your site config here */
   plugins: [
+    // PWA
+    // 配置一个 manifest.webmanifest 文件
+    // https://www.gatsbyjs.cn/packages/gatsby-plugin-manifest/
+    {
+      resolve: `gatsby-plugin-manifest`,
+      // 插件配置项：https://www.gatsbyjs.cn/packages/gatsby-plugin-manifest/?=gatsby-plugin-manifest#automatic-mode
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // PWA
+    // 添加离线支持:使 Gatsby 站点能够离线运行，并通过创建一个 service worker 使你的站点在糟糕网络状况下受到的影响更小。
+    // https://www.gatsbyjs.cn/packages/gatsby-plugin-offline/
+    // 离线插件应该放在清单插件 之后。以便离线插件可以缓存清单插件创建的 manifest.webmanifest。
+    `gatsby-plugin-offline`,
+    // SEO
+    `gatsby-plugin-react-helmet`,
     // 数据源插件
     {
       resolve: `gatsby-source-filesystem`,
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      {/* 通过向<SEO> 组件传入 props，可以实时更改博文的元数据。 */}
+      {/* 在这种情况下，将使用博客标题 title 和 excerpt（如果存在于博客帖子markdown文件中） */}
+      {/* 代替 gatsby-config.js 文件中默认 siteMetadata` 属性。 */}
+      {/* https://www.gatsbyjs.cn/docs/add-seo-component/ */}
+      {/* https://github.com/nfl/react-helmet#example */}
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -21,6 +28,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
     }
   }
 `
