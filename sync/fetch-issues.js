fbe8f3c6c00910ac5519b3012f5d760f5bab404c
shanejix <shanejix@hotmail.com>
2021-01-24 21:39:12 +0800

    🚩 add imgs spider

--- sync/fetch-issues.js
+++ sync/fetch-issues.js
@@ -1,3 +1,4 @@
+const request = require("request")
 const fs = require("fs")
 const path = require("path")
 const axios = require("axios")
@@ -6,6 +7,8 @@ const { rebuild } = require("./utils")
 // https://api.github.com/repos/shanejix/shanejix.github.io/issues
 const github_username = "shanejix"
 const github_rep = "shanejix.github.io"
+
+const imgUrlReg = /((http|https):\/\/)+(\w+\.)+(\w+)[%20\w\/\.\-]*(jpg|gif|png)/gi
 
 const GITHUB_BASE_URL = "https://api.github.com"
 const mdDir = path.resolve(__dirname, "../posts")
@@ -27,15 +30,30 @@ module.exports = async () => {
       post => post.state === "open" && post.author_association === "OWNER"
     )
 
-    // console.log(posts);
-
     // 按照title创建文件夹和index.md
     posts.forEach(post => {
-      const body = injectHeader(post)
+      let body = injectHeader(post)
+      const postImgUrls = getPostImgUrls(post)
+
       const postDir = `${mdDir}/${post.title}`
       const postFilePath = path.join(postDir, `${post.title}.md`);
 
+      console.log('postImgUrls', postImgUrls);
+      // make dirtory
       fs.mkdirSync(postDir);
+      // make images
+      postImgUrls.forEach((imgUrl) => {
+        const imgFileName = imgUrl.split('/').pop();
+        request(imgUrl).pipe(
+          fs.createWriteStream(`${postDir}/${imgFileName}`).on('close', err => { err && console.log('write imgs error', err) })
+        )
+      })
+      // rewrite images url using local path
+      body = body.replace(imgUrlReg, res => {
+        let filename = res.split('/').pop()
+        return `./${filename}`
+      })
+      // write post body in markdown file
       fs.writeFileSync(postFilePath, body, "utf8");
     })
 
@@ -68,4 +86,22 @@ function injectHeader(post) {
 ${post.body}`
 
   return result
+}
+
+/**
+ * get single post imgs list
+ *
+ * @param {*} post
+ * @return {*} 
+ */
+function getPostImgUrls(post) {
+  const imgUrlList = []
+  let matchedImgUrlList
+
+  const postBody = post.body || ''
+
+  matchedImgUrlList = [...postBody.matchAll(imgUrlReg)]
+  matchedImgUrlList.forEach(item => imgUrlList.push(item[0]))
+
+  return imgUrlList
 }
